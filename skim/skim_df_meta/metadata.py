b4272e9e9ee227e47641ee145eabac8a2d04fe88
fbraza <fbraza@tutanota.com>
2021-03-30 22:17:15 +0200

    refactor: moves piece of code from the data summary into a python class

--- skim/skim_df_meta/metadata.py
+++ skim/skim_df_meta/metadata.py
@@ -1,34 +1,50 @@
 import pandas as pd
-from typing import List
 
 
-class SkimDfMeta:
-    def __init__(self):
-        self.unique_types = ...     # [dtypes]
-        self.columns = ...          # [columns]
-        self.numeric_summary = ...  # a dictionnary with all data for each columns
-        self.dfect_summary = ...    # a dictionnary with all data for each columns
-        self.dates_summary = ...    # a dictionnary with all data for each columns
+class SkimDfDataSummary:
+    def __init__(self, df: pd.DataFrame):
+        self.types = [str(dtype) for dtype in df.dtypes.unique()]
+        self.summary = self.data_summary(df)
+        self.numeric = self.numeric_summary(df) if self._has_numeric(df) else None
 
-    def _get_dtypes(self, df: pd.DataFrame) -> List:
-        return [str(dtype) for dtype in df.dtypes.unique()]
+    def data_summary(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Function defined to return a concatenated dataframe containing
+        details about the number of row and columns and the column dtype
+        frequency of the passed dataframe or series object"""
+        def _shape(df: pd.DataFrame) -> pd.DataFrame:
+            """Function defined to return a dataframe with details about
+            the number of row and columns"""
+            row, col = df.shape
+            return pd.DataFrame(data=[[row], [col]],
+                                columns=['Values'],
+                                index=['Number of rows', 'Number of columns'])
 
-    def _shape(self, df: pd.DataFrame) -> pd.DataFrame:
-        """Function defined to return a dataframe with details about
-        the number of row and columns"""
-        row, col = df.shape
-        return pd.DataFrame(data=[[row], [col]],
-                            columns=['Values'],
-                            index=['Number of rows', 'Number of columns'])
+        def _dtypes_freq(df: pd.DataFrame) -> pd.DataFrame:
+            """Function defined to return a dataframe with details about
+            the pandas dtypes frequency"""
+            dtypes, counter = [str(dtype) for dtype in df.dtypes.values], {}
+            for dtype in dtypes:
+                if dtype in counter.keys():
+                    counter[dtype] += 1
+                else:
+                    counter[dtype] = 0
+            values = [[value] for value in counter.values()]
+            return pd.DataFrame(data=values, columns=['Values'], index=dtypes)
+        return pd.concat([_shape(df), _dtypes_freq(df)])
 
-    def _dtypes_freq(self, df: pd.DataFrame) -> pd.DataFrame:
-        """Function defined to return a dataframe with details about
-        the pandas dtypes frequency"""
-        dtypes, counter = self._get_dtypes(df), {}
-        for dtype in dtypes:
-            if dtype in counter.keys():
-                counter[dtype] += 1
-            else:
-                counter[dtype] = 0
-        values = [[value] for value in counter.values()]
-        return pd.DataFrame(data=values, columns=['Values'], index=dtypes)
+    def numeric_summary(self, df: pd.DataFrame) -> pd.DataFrame:
+        dtypes = filter(_is_numeric, self.types)
+        pass
+
+    def _has_numeric(self, df: pd.DataFrame) -> bool:
+        for element in self.types:
+            if self._is_numeric(element):
+                return True
+
+    def _is_numeric(self, dtype: str) -> bool:
+        types = [
+            'int8', 'int16','int32', 'int64',
+            'uint8', 'uint16', 'uint32', 'uint64',
+            'float16', 'float32', 'float64', 'float128'
+        ]
+        return dtype in types
