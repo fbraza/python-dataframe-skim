bc865affe83991862f2b42c53687196a8f6876dd
fbraza <fbraza@tutanota.com>
2021-06-13 11:04:04 +0200

    test: test for numeric and object Skim

--- tests/test_skim.py
+++ tests/test_skim.py
@@ -1,6 +1,6 @@
-from skim.skim import check_types, data_summary
 import unittest
 import pandas as pd
+from skim.skim_df_meta.metadata import SkimDfDataSummary
 
 
 class TestObjectTypes(unittest.TestCase):
@@ -12,38 +12,70 @@ class TestObjectTypes(unittest.TestCase):
         self.df = pd.DataFrame(self.dc)
 
     def test_is_dataframe(self) -> None:
-        # initialize variables
         with_dc = True if isinstance(self.dc, pd.DataFrame) else False
         with_ls = True if isinstance(self.ls, pd.DataFrame) else False
         with_st = True if isinstance(self.st, pd.DataFrame) else False
         with_sr = True if isinstance(self.sr, pd.DataFrame) else False
         with_df = True if isinstance(self.df, pd.DataFrame) else False
-        # test values
         self.assertFalse(with_dc)
         self.assertFalse(with_ls)
         self.assertFalse(with_st)
         self.assertFalse(with_sr)
         self.assertTrue(with_df)
 
-    def test_exception_raised_if_not_good_type(self) -> None:
-        self.assertRaises(AttributeError, check_types, self.ls)
-
 
 class TestDataSummary(unittest.TestCase):
-    def setUp(self) -> None:
-        self.df = pd.DataFrame(
-            data=[[1, 'a'], [2, 'b'], [3, 'c']],
-            columns=['numbers', 'letters']
-            )
+    def setUp(self):
+        data, cols = [[1, 'a'], [2, 'b'], [3, 'c']], ['numbers', 'letters']
+        self.df1 = pd.DataFrame(data=data, columns=cols)
 
-    def test_data_summary(self) -> None:
-        result = data_summary(self.df)
-        expected = pd.DataFrame(
-            data=[[3], [2], [1], [1]],
-            columns=['Values'],
-            index=['Number of rows', 'Number of columns', 'int64', 'object']
-            )
+    def test_shape(self):
+        cols, idx = ['Values'], ['Number of rows', 'Number of columns']
+        data = [[3], [2]]
+        row, col = self.df1.shape
+        expected = pd.DataFrame(data=data, columns=cols, index=idx)
+        result = pd.DataFrame(data=[[row], [col]], columns=cols, index=idx)
+        self.assertTrue(result.equals(expected))
+
+    def test_type_freq(self):
+        counter, types = {}, self.df1.dtypes
+        for dtype in types:
+            tmp = str(dtype)
+            if tmp in counter.keys():
+                counter[tmp] += 1
+            else:
+                counter[tmp] = 1
+        values = [[value] for value in counter.values()]
+        cols, idx = ['Values'], list(counter.keys())
+        result = pd.DataFrame(data=values, columns=cols, index=idx)
+        # expected data
+        data = [[1], [1]]
+        expected = pd.DataFrame(data=data, columns=cols, index=idx)
+        self.assertTrue(result.equals(expected))
+
+    def test_data_summary(self):
+        data, cols = [[3], [2], [1], [1]], ['Values']
+        idx = ['Number of rows', 'Number of columns', 'int64', 'object']
+        result = SkimDfDataSummary(self.df1).data_summary()
+        expected = pd.DataFrame(data=data, columns=cols, index=idx)
+        print(result)
         self.assertTrue(expected.equals(result))
+
+
+class TestNumericSummary(unittest.TestCase):
+    def setUp(self):
+        data = [[1, 'a', 3.4], [2, 'b', 5.5], [3, 'c', 6.8]]
+        cols = ['integer', 'letters', 'floats']
+        self.df2 = pd.DataFrame(data=data, columns=cols)
+
+    def test_select_numeric(self):
+        types = ['int8', 'int16', 'int32', 'int64',
+                 'uint8', 'uint16', 'uint32', 'uint64',
+                 'float16', 'float32', 'float64', 'float128']
+        data, cols = [[1, 3.4], [2, 5.5], [3, 6.8]], ['integer', 'floats']
+        result = self.df2.select_dtypes(include=types)
+        expected = pd.DataFrame(data=data, columns=cols)
+        self.assertTrue(result.equals(expected))
 
 
 if __name__ == '__main__':
