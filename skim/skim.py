c3fee571b507c7754686cd417ea58845c9086d08
fbraza <fbraza@tutanota.com>
2021-06-13 11:03:34 +0200

    feat: add the numeric feature for skim

--- skim/skim.py
+++ skim/skim.py
@@ -3,11 +3,7 @@ from typing import Any, List, Union
 from collections import Counter
 
 
-def skim_df(
-    obj: Union[pd.DataFrame, pd.Series],
-    column: str = None,
-    columns: List[str] = None
-) -> List:
+def skim_df() -> List:
     """Summarize data present in dataframe
 
     Raise
@@ -16,38 +12,3 @@ def skim_df(
     as a parameter
     """
     pass
-
-
-def is_dataframe(obj: Any) -> bool:
-    """Check if passed object is a pandas Dataframe"""
-    return True if isinstance(obj, pd.DataFrame) else False
-
-
-def data_summary(obj: pd.DataFrame) -> pd.DataFrame:
-    """Function defined to return a concatenated dataframe containing
-    details about the number of row and columns and the column dtype
-    frequency of the passed dataframe or series object"""
-    def __shape(obj: pd.DataFrame) -> pd.DataFrame:
-        """Function defined to return a dataframe with details about
-        the number of row and columns"""
-        row, col = obj.shape
-        return pd.DataFrame(data=[[row], [col]],
-                            columns=['Values'],
-                            index=['Number of rows', 'Number of columns'])
-
-    def __dtype_freq(obj: pd.DataFrame) -> pd.DataFrame:
-        """Function defined to return a dataframe with details about
-        the pandas dtypes frequency"""
-        types = [str(key) for key in Counter(obj.dtypes).keys()]
-        values = [[value] for value in Counter(obj.dtypes).values()]
-        return pd.DataFrame(data=values, columns=['Values'], index=types)
-    return pd.concat([__shape(obj), __dtype_freq(obj)])
-
-
-def summary_by_type(obj: pd.DataFrame) -> None:
-    types = [str(key) for key in Counter(obj.dtypes).keys()]
-
-
-def numeric_summary(obj: pd.DataFrame):
-    columns = obj.columns
-
--- skim/skim_df_meta/metadata.py
+++ skim/skim_df_meta/metadata.py
@@ -1,50 +1,94 @@
 import pandas as pd
+import numpy as np
+from typing import Union
 
 
 class SkimDfDataSummary:
     def __init__(self, df: pd.DataFrame):
+        self.df = df
         self.types = [str(dtype) for dtype in df.dtypes.unique()]
-        self.summary = self.data_summary(df)
-        self.numeric = self.numeric_summary(df) if self._has_numeric(df) else None
+        self.summary = self.data_summary()
+        self.numeric = self.numeric_summary()
 
-    def data_summary(self, df: pd.DataFrame) -> pd.DataFrame:
-        """Function defined to return a concatenated dataframe containing
+    def data_summary(self) -> pd.DataFrame:
+        """
+        Function defined to return a concatenated dataframe containing
         details about the number of row and columns and the column dtype
-        frequency of the passed dataframe or series object"""
-        def _shape(df: pd.DataFrame) -> pd.DataFrame:
-            """Function defined to return a dataframe with details about
-            the number of row and columns"""
-            row, col = df.shape
+        frequency of the passed dataframe or series object
+        """
+        def _shape() -> pd.DataFrame:
+            """
+            Function defined to return a dataframe with details about
+            the number of row and columns
+            """
+            row, col = self.df.shape
             return pd.DataFrame(data=[[row], [col]],
                                 columns=['Values'],
                                 index=['Number of rows', 'Number of columns'])
 
-        def _dtypes_freq(df: pd.DataFrame) -> pd.DataFrame:
-            """Function defined to return a dataframe with details about
-            the pandas dtypes frequency"""
-            dtypes, counter = [str(dtype) for dtype in df.dtypes.values], {}
-            for dtype in dtypes:
-                if dtype in counter.keys():
-                    counter[dtype] += 1
+        def _dtypes_freq() -> pd.DataFrame:
+            """
+            Function defined to return a dataframe with details about
+            the pandas dtypes frequency
+            """
+            counter, types = {}, self.df.dtypes
+            for dtype in types:
+                tmp = str(dtype)
+                if tmp in counter.keys():
+                    counter[tmp] += 1
                 else:
-                    counter[dtype] = 0
+                    counter[tmp] = 1
             values = [[value] for value in counter.values()]
-            return pd.DataFrame(data=values, columns=['Values'], index=dtypes)
-        return pd.concat([_shape(df), _dtypes_freq(df)])
+            return pd.DataFrame(
+                data=values,
+                columns=['Values'],
+                index=list(counter.keys())
+                )
+        return pd.concat([_shape(), _dtypes_freq()])
 
-    def numeric_summary(self, df: pd.DataFrame) -> pd.DataFrame:
-        dtypes = filter(_is_numeric, self.types)
-        pass
+    def numeric_summary(self, bins: int = 10) -> Union[pd.DataFrame, None]:
+        # check if dataframe has numeric values
+        df = self._select_numeric()
+        if df.columns.size == 0:
+            return None
+        # initialize the calculations and record in a dictionnary
+        cols = list(df.columns)
+        q0, q25, q50, q75, q100 = df.quantile([.0, .25, .50, .75, 1.0]).values
+        summary_record = {
+            "variable": cols[:4],
+            "n_missing": [df[col].isnull().sum() for col in cols],
+            "mean": [df[col].mean() for col in cols],
+            "sd": [df[col].mean() for col in cols],
+            "q0": q0,
+            "q25": q25,
+            "q50": q50,
+            "q75": q75,
+            "q100": q100,
+            "hist": [self._hist(df[col], bins=bins) for col in cols]
+        }
+        return pd.DataFrame(summary_record)
 
-    def _has_numeric(self, df: pd.DataFrame) -> bool:
-        for element in self.types:
-            if self._is_numeric(element):
-                return True
+    def _select_numeric(self) -> pd.DataFrame:
+        """
+        Function defined to select all DataFrame columns with a numeric dtype
+        """
+        def _is_numeric(dtype: str) -> bool:
+            types = [
+                'int8', 'int16', 'int32', 'int64',
+                'uint8', 'uint16', 'uint32', 'uint64',
+                'float16', 'float32', 'float64', 'float128'
+            ]
+            return dtype in types
+        dtypes = list(filter(_is_numeric, self.types))
+        return self.df.select_dtypes(include=dtypes)
 
-    def _is_numeric(self, dtype: str) -> bool:
-        types = [
-            'int8', 'int16','int32', 'int64',
-            'uint8', 'uint16', 'uint32', 'uint64',
-            'float16', 'float32', 'float64', 'float128'
-        ]
-        return dtype in types
+    def _hist(self, data: pd.DataFrame, bins: int = 10) -> str:
+        bars = u' ▁▂▃▄▅▆▇█'
+        n, _ = np.histogram(data, bins=bins)
+        temp = n * (len(bars) - 1) // (max(n))
+        hist = u"".join(bars[i] for i in temp)
+        return hist
+
+
+test = pd.read_csv("skim/iris.csv")
+print(SkimDfDataSummary(test).numeric_summary())
