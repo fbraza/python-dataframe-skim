7d01fcb1caa0f2a3f9b38142ad1fae57dff0c466
fbraza <dataroots@localhost.localdomain>
2021-07-15 11:11:38 +0200

    refactor: refactor the attributes skimmer to store all dictionnary fo statistic

--- skimpy/skim.py
+++ skimpy/skim.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import helpers as H
-from typing import Dict, List, Union
+from typing import Dict, List, Optional, Union
 from rich.console import Console
 from rich.table import Table
 from collections import defaultdict
@@ -19,50 +19,58 @@ class Skimer:
         self.name = name
         self.rows = data.shape[0]
         self.cols = data.shape[1]
-        self._sumskim: Dict[str, List] = defaultdict(list)
-        self._numskim: Dict[str, List] = defaultdict(list)
+        # have a a attribute called skimmers that is a defaultdict(dict)
+        self.skimmer: Dict[str, Dict[str, List]] = defaultdict(dict)
+    
+    def set_skimmers(self):
+        self._set_sumskim()
+        self._set_numskim()
 
     def _set_sumskim(self):
         """
         TODO
         """
-        self._sumskim[" "] = ["Number of rows", "Number of columns"]
-        self._sumskim["Values"] = [str(self.rows), str(self.cols)]
+        _sumskim: Dict[str, List] = defaultdict(list)
+        _sumskim[" "] = ["Number of rows", "Number of columns"]
+        _sumskim["Values"] = [str(self.rows), str(self.cols)]
         dtypes_freq = H.count_types_freq(self.data)
         for k, v in dtypes_freq.items():
-            self._sumskim[" "].append("The frequency of {}".format(k))
-            self._sumskim["Values"].append(str(v))
+            _sumskim[" "].append("The frequency of {}".format(k))
+            _sumskim["Values"].append(str(v))
+        self.skimmer["summary"] = _sumskim
 
-    def _set_numskim(self):
+    def _set_numskim(self) -> Optional[None]:
         """
         TODO
         """
+        _numskim: Dict[str, List] = defaultdict(list)
         data = H.columns_with_type(self.data, "numeric")
         cols = H.columns_to_list(data)
         if not cols:
-            self._numskim["Empty"] = [True]
+            self.skimmer["numeric"] = {}
         # intialize some variables
         q0, q25, q50, q75, q100 = H.calculate_quantiles(data)
         _min, _max = H.columns_min_max(data, cols)
         # set the _numeric dictionnary
-        self._numskim["Variable"] = cols
-        self._numskim["Count_total"] = H.count_columns_values(data, cols)
-        self._numskim["Count_missing"] = H.count_missing_values(data, cols)
-        self._numskim["Min"] = _min
-        self._numskim["Max"] = _max
-        self._numskim["Mean"] = H.calculate_means(data, cols)
-        self._numskim["Std"] = H.calculate_sd(data, cols)
-        self._numskim["Q0"] = q0
-        self._numskim["Q25"] = q25
-        self._numskim["Q50"] = q50
-        self._numskim["Q75"] = q75
-        self._numskim["Q100"] = q100
-        self._numskim["Skew"] = H.calculate_skew(data, cols)
-        self._numskim["Kurt"] = H.calculate_kurosis(data, cols)
-        self._numskim["Dist"] = H.draw_distribitions(data, cols)
+        _numskim["Variable"] = cols
+        _numskim["Count_total"] = H.count_columns_values(data, cols)
+        _numskim["Count_missing"] = H.count_missing_values(data, cols)
+        _numskim["Min"] = _min
+        _numskim["Max"] = _max
+        _numskim["Mean"] = H.calculate_means(data, cols)
+        _numskim["Std"] = H.calculate_sd(data, cols)
+        _numskim["Q0"] = q0
+        _numskim["Q25"] = q25
+        _numskim["Q50"] = q50
+        _numskim["Q75"] = q75
+        _numskim["Q100"] = q100
+        _numskim["Skew"] = H.calculate_skew(data, cols)
+        _numskim["Kurt"] = H.calculate_kurosis(data, cols)
+        _numskim["Dist"] = H.draw_distribitions(data, cols)
+        self.skimmer["numeric"] = _numskim
 
 
-def skim(data: pd.DataFrame) -> None:
+def skim(data: pd.DataFrame, choices: List[str] = None) -> None:
     """
     Summarize data present in a Pandas DataFrame
 
@@ -76,17 +84,17 @@ def skim(data: pd.DataFrame) -> None:
     as an argument
     """
     if not isinstance(data, pd.DataFrame):
-        raise TypeError("The skim functions only accepts Pandas DataFrames")
+        raise TypeError("For now,the skim functions only accepts Pandas DataFrames")
+    # instantiate the rich Console object
     console = Console()
-    # sk_summary = Skimer(data, name).summary
+    # instantiate the skimmer object
     skimmer = Skimer(data)
-    skimmer._set_numskim()
-    numeric = skimmer._numskim
+    skimmer.set_skimmers()
     # sk_object: TODO
     # sk_dates: TODO
     # sk_category: TODO
 
-    def _build_num(summary: Union[dict, None]) -> Union[Table, None]:
+    def _build_rich_grid(summary: Union[dict, None]) -> Union[Table, None]:
         if summary is None:
             return
         else:
@@ -99,24 +107,10 @@ def skim(data: pd.DataFrame) -> None:
             for row in rows:
                 grid.add_row(*row)
             return grid
-
-#    def _build_sum(summary: List) -> Table:
-#        grid = Table(show_header=True, header_style="bold magenta")
-#        columns = ["", "Values"]
-#        rows = summary
-#        for col in columns:
-#            grid.add_column(col)
-#        for row in rows:
-#            grid.add_row(*row)
-#        return grid
-#
-#    console.print(" [u][bold]Data Summary[/bold][/u]")
-#    console.print(_build_sum(sk_summary))
-#    print()
-    console.print(" [u][bold]Variable Type: Numeric[/bold][/u]")
-    console.print(_build_num(numeric))
+    for key, value in skimmer.skimmer.items():
+        console.print(" [u][bold]Variable Type: Numeric[/bold][/u]")
+        console.print(_build_rich_grid(value))
 
 
 data = pd.read_csv("tests/data/iris.csv")
 skim(data)
-
